e6fafc3c4d51fec25e77518df5ae385a3854deeb
Alan Matthews <matthewsa1320@gmail.com>
2024-05-01 17:30:55 -0400

    Mark as read logic refactors

--- day-5/main.js
+++ day-5/main.js
@@ -2,14 +2,38 @@
 
 const notifications = document.querySelector(".notifications");
 const btnRead = document.querySelector(".btn-read");
-const unread = document.querySelectorAll(".unread");
+const unread = document.getElementsByClassName("unread");
 
 btnRead.addEventListener("click", _markAllAsRead);
 
+Array.from(unread).map((notification) => {
+  notification.addEventListener(
+    "click",
+    (notification.markAsRead = function () {
+      _markAsRead(notification);
+    }),
+    { once: true }
+  );
+});
+
 function _markAllAsRead() {
   notifications.textContent = 0;
-  unread.forEach((notification) => {
-    notification.classList.remove("bg-[--very-light-gray-blue]");
-    notification.querySelector("svg").remove();
+  btnRead.classList.add("hidden");
+  Array.from(unread).forEach((notification) => {
+    _removeStyles(notification);
+    notification.removeEventListener("click", notification.markAsRead);
   });
+}
+
+function _markAsRead(notification) {
+  notifications.textContent -= 1;
+  _removeStyles(notification);
+  console.log(notifications.textContent);
+  if (notifications.textContent === "0") btnRead.classList.add("hidden");
+}
+
+function _removeStyles(notification) {
+  notification.classList.remove("bg-[--very-light-gray-blue]");
+  notification.querySelector("svg").remove();
+  notification.classList.remove("unread");
 }
