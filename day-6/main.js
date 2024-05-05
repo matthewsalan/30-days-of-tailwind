7562603afb67a531a233b2984087840afa4c7ad3
Alan Matthews <matthewsa1320@gmail.com>
2024-05-05 19:28:22 -0400

    Small refactors

--- day-6/main.js
+++ day-6/main.js
@@ -1,1 +1,16 @@
 "use-strict";
+
+const btnBookmark = document.querySelector(".btn-bookmark");
+const iconBookmark = document.querySelector(".bookmark-icon");
+const bookMarkCircle = document.querySelector(".bookmark-circle");
+const bookmarkText = document.querySelector(".bookmark-text");
+
+btnBookmark.addEventListener("click", _bookmark);
+
+function _bookmark() {
+  btnBookmark.classList.add("text-[--dark-cyan]");
+  btnBookmark.classList.remove("text-[--dark-gray]");
+  bookmarkText.textContent = "Bookmarked";
+  iconBookmark.classList.add("svg-bookmark");
+  bookMarkCircle.classList.add("svg-circle");
+}
