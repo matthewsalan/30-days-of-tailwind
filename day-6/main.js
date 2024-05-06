b2f6571511cac9c833940576a27e7a237bea5f6b
Alan Matthews <matthewsa1320@gmail.com>
2024-05-06 16:46:57 -0400

    Add intial modal

--- day-6/main.js
+++ day-6/main.js
@@ -1,11 +1,18 @@
 "use-strict";
 
-const btnBookmark = document.querySelector(".btn-bookmark");
+const btnBookmark = document.querySelector(".btn--bookmark");
 const iconBookmark = document.querySelector(".bookmark-icon");
 const bookMarkCircle = document.querySelector(".bookmark-circle");
 const bookmarkText = document.querySelector(".bookmark-text");
+const btnRewards = document.querySelectorAll(".btn--reward");
+const rewardModal = document.querySelector(".modal--reward");
+const btnRadio = document.querySelector(".btn--radio");
 
 btnBookmark.addEventListener("click", _bookmark);
+
+btnRewards.forEach((reward) => reward.addEventListener("click", _openModal));
+
+// btnRadio.addEventListener("click", _markSelectedBtn);
 
 function _bookmark() {
   btnBookmark.classList.add("text-[--dark-cyan]");
@@ -13,4 +20,12 @@ function _bookmark() {
   bookmarkText.textContent = "Bookmarked";
   iconBookmark.classList.add("svg-bookmark");
   bookMarkCircle.classList.add("svg-circle");
+}
+
+function _openModal() {
+  rewardModal.classList.remove("hidden");
+}
+
+function _markSelectedBtn() {
+  btnRadio.classList.add("bg-[--moderate-cyan]");
 }
